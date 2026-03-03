ed71a51c8d76e16602699e4aa340fdde66c900da
QUICK <264386559+quick018@users.noreply.github.com>
2026-03-03 23:16:36 +0530

    Add files via upload

---
+++ 2_Exercise Solved/Exercise 2.py
@@ -0,0 +1,9 @@
+import time
+timecheck = time.strftime('%H: %M: %S')
+print(timecheck)
+timecheck = time.strftime('%H:')
+print(timecheck)
+timecheck = time.strftime('%M:')
+print(timecheck)
+timecheck = time.strftime('%S:')
+print(timecheck)
