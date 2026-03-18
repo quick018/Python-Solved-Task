a98d5cafd50a68a7d6dcde66d43e45fba27884b0
QUICK <264386559+quick018@users.noreply.github.com>
2026-03-18 22:52:56 +0530

    Add files via upload
    Added a comment indicating authorship to the program.

--- Raise Quick Quiz solved/main.py
+++ Raise Quick Quiz solved/main.py
@@ -1,8 +1,9 @@
 # Quick Quiz From and create a program if QUITE is UPPER so print else raise value error 
+#    Which was made by me
+
 
 a = input("Enter the value: ")
 if 'QUITE' in a:
     print("QUITE")
 else:    
     raise ValueError("This is not UPPERLETTER")
-
