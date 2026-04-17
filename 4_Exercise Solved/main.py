dbe5c9d9d2fc5ffa8bb3df8c923dbe0f9feba7a2
QUICK <264386559+quick018@users.noreply.github.com>
2026-04-17 14:25:44 +0530

    1st changed
    Refactor coding logic to include random string generation for encoding. Adjust decoding logic for words with length 9 or more.

--- 4_Exercise Solved/main.py
+++ 4_Exercise Solved/main.py
@@ -1,15 +1,23 @@
 #    CODING this was made by me
 
+import random
+import string
+
+
 ms = input("Enter the message: ")
 words = ms.split(" ")
 coding = input("1 for Coding or 0 for Decoding: ")
 coding = True if(coding == "1") else False
+
+def get_random_vlaue():
+    return "".join(random.choices(string.ascii_lowercase, k=3))
+
 if(coding):
     neword = []
     for word in words:
       if(len(word)>=3):
-        r1 = "opp"
-        r2 = "miu"
+        r1 = get_random_vlaue()
+        r2 = get_random_vlaue()
         msnew = r1 + word[1:] + word[0] + r2
         neword.append(msnew)
       else:
@@ -21,7 +29,7 @@ if(coding):
 else:
    neword = []
    for word in words:
-     if(len(word)>=3):
+     if(len(word)>=9):
        msnew = word[3:-3]
        msnew = msnew[-1] + msnew[:-1]
        neword.append(msnew)
